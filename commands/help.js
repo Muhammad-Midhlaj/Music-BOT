dbc5cdd76435f61ea5cf01eea74aafb75c0f654f
procoder4 <procoder.4tech@gmail.com>
2020-07-24 18:23:10 +0530

    first commit

---
+++ commands/help.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  description: "Display all commands and descriptions",
+  execute(message) {
+    /*
+    let commands = message.client.commands.array();
+    commands.shift()
+    let helpEmbed = new MessageEmbed()
+      .setTitle("Aakasha Vani Help Line")
+      .setDescription("List of all commands")
+      .setColor("#F8AA2A");
+
+    commands.forEach((cmd) => {
+      helpEmbed.addField(
+        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
+        `${cmd.description}`,
+        true
+      );
+    });
+
+    helpEmbed.setTimestamp();
+
+    return message.channel.send(helpEmbed);
+    */
+   //Music-Help Channel ID = 732460163495100428
+   message.reply("Please see <#732460163495100428> for **Music Bot** commands.");
+  }
+};
